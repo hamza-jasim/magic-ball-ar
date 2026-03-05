f9537bf76441290a6542c76d5b7164fe3191dc3a
hamza-jasim <hamza201317@gmail.com>
2026-03-05 06:45:23 -0700

    Add constants/colors.ts

---
+++ constants/colors.ts
@@ -0,0 +1,26 @@
+const gold = "#D4AF37";
+const deepGold = "#B8960C";
+const lightGold = "#F0D060";
+const midnight = "#080810";
+const darkPurple = "#1A0A2E";
+const purple = "#4B0082";
+
+export default {
+  gold,
+  deepGold,
+  lightGold,
+  midnight,
+  darkPurple,
+  purple,
+  text: "#F5F0E8",
+  textSecondary: "#A89060",
+  surface: "#100820",
+  border: "#2A1A40",
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: gold,
+    tabIconDefault: "#ccc",
+    tabIconSelected: gold,
+  },
+};
